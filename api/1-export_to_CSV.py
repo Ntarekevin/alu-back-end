53b14dc4cd59b8c23d992d6cf7f72c8c53eec1c8
Ntarekevin <k.nyawakira@alustudent.com>
2025-07-09 11:38:12 +0200

    innit

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
-"""Exports employee task data to CSV format"""
-import csv
+"""This module gathers data from an API and displays employee's completed tasks"""
 import requests
 import sys
+
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
 
-    # Get user info
+    # Fetch user data
     user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
     user = requests.get(user_url).json()
-    username = user.get("username")
+    employee_name = user.get("name")
 
-    # Get user todos
+    # Fetch todos
     todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
     todos = requests.get(todos_url).json()
 
-    # Create CSV file
-    file_name = f"{user_id}.csv"
-    with open(file_name, mode="w", newline='') as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for task in todos:
-            writer.writerow([
-                user_id,
-                username,
-                str(task.get("completed")),
-                task.get("title")
-            ])
-init
+    # Get completed tasks
+    completed_tasks = [task for task in todos if task.get("completed")]
+    total_tasks = len(todos)
+
+    # Print result
+    print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):")
+    for task in completed_tasks:
+        print(f"\t {task.get('title')}")
