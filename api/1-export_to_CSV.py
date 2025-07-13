087082d2ede2e0071ec60fe2558a98f7a42720ae
Ntarekevin <k.nyawakira@alustudent.com>
2025-07-13 20:54:52 +0200

    done

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -16,15 +16,13 @@ if __name__ == "__main__":
     todos_resp = requests.get(todos_url)
 
     if user_resp.status_code != 200 or todos_resp.status_code != 200:
-        print("Error fetching data")
         sys.exit(1)
 
     user_data = user_resp.json()
     username = user_data.get('username')
-
     todos = todos_resp.json()
-    file_name = f"{user_id}.csv"
 
+    file_name = f"{user_id}.csv"
     with open(file_name, mode='w', newline='') as csv_file:
         writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         for task in todos:
