02aec3ee0971c99aadb80355958b3ba8ffcaa2d0
Ntarekevin <k.nyawakira@alustudent.com>
2025-07-13 20:36:31 +0200

    new update

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,39 +1,25 @@
 #!/usr/bin/python3
-"""
-Exports all tasks for a given employee ID to a CSV file.
-"""
-
 import csv
 import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2 or not sys.argv[1].isdigit():
-        print("Usage: ./1-export_to_CSV.py <employee_id>")
-        sys.exit(1)
+    user_id = sys.argv[1]
 
-    employee_id = int(sys.argv[1])
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-
+    # Fetch user information
+    user_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
     user_response = requests.get(user_url)
-    if user_response.status_code != 200:
-        print("User not found")
-        sys.exit(1)
-
     user_data = user_response.json()
-    username = user_data.get("username")
+    username = user_data.get('username')
 
+    # Fetch tasks for the user
+    todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={user_id}'
     todos_response = requests.get(todos_url)
     todos = todos_response.json()
 
-    filename = f"{employee_id}.csv"
-    with open(filename, mode="w", newline="", encoding="utf-8") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+    # Export to CSV
+    file_name = f"{user_id}.csv"
+    with open(file_name, mode='w', newline='') as csv_file:
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         for task in todos:
-            writer.writerow([
-                employee_id,
-                username,
-                task.get("completed"),
-                task.get("title")
-            ])
+            writer.writerow([user_id, username, task.get('completed'), task.get('title')])
