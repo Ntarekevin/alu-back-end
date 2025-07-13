bb4f0b2a59262eae06e98a18a44cd0cab4f38bb8
Ntarekevin <k.nyawakira@alustudent.com>
2025-07-13 20:46:26 +0200

    new update

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,26 +1,36 @@
 #!/usr/bin/python3
-"""this me importing modules for this project"""
+"""Exports user's TODO list data to a CSV file"""
+
 import csv
 import requests
 import sys
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
+    base_url = 'https://jsonplaceholder.typicode.com'
 
-    # Fetch user information
-    user_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
+    user_url = f'{base_url}/users/{user_id}'
+    todos_url = f'{base_url}/todos?userId={user_id}'
+
+    user_resp = requests.get(user_url)
+    todos_resp = requests.get(todos_url)
+
+    if user_resp.status_code != 200 or todos_resp.status_code != 200:
+        print("Error fetching data")
+        sys.exit(1)
+
+    user_data = user_resp.json()
     username = user_data.get('username')
 
-    # Fetch tasks for the user
-    todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={user_id}'
-    todos_response = requests.get(todos_url)
-    todos = todos_response.json()
-
-    # Export to CSV
+    todos = todos_resp.json()
     file_name = f"{user_id}.csv"
+
     with open(file_name, mode='w', newline='') as csv_file:
         writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         for task in todos:
-            writer.writerow([user_id, username, task.get('completed'), task.get('title')])
+            writer.writerow([
+                user_id,
+                username,
+                str(task.get('completed')),
+                task.get('title')
+            ])
