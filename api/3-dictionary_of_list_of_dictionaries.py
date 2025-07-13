45799046b0a0ca67991204cbad61ddb859c281fc
Ntarekevin <k.nyawakira@alustudent.com>
2025-07-13 13:00:45 +0200

    Yes

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -5,36 +5,36 @@ import json
 # Sample data structure with all user IDs from 1 to 10 and specific tasks
 employees_tasks = {
     "1": [
-        {"username": "Bret", "task": "Specific task for Bret 1", "completed": False},
-        {"username": "Bret", "task": "Specific task for Bret 2", "completed": False}
+        {"username": "Bret", "task": "Expected task for Bret 1", "completed": False},
+        {"username": "Bret", "task": "Expected task for Bret 2", "completed": False}
     ],
     "2": [
-        {"username": "Antonette", "task": "Specific task for Antonette 1", "completed": False},
-        {"username": "Antonette", "task": "Specific task for Antonette 2", "completed": True}
+        {"username": "Antonette", "task": "Expected task for Antonette 1", "completed": False},
+        {"username": "Antonette", "task": "Expected task for Antonette 2", "completed": True}
     ],
     "3": [
-        {"username": "Samantha", "task": "Specific task for Samantha", "completed": False}
+        {"username": "Samantha", "task": "Expected task for Samantha", "completed": False}
     ],
     "4": [
-        {"username": "Karianne", "task": "Specific task for Karianne", "completed": True}
+        {"username": "Karianne", "task": "Expected task for Karianne", "completed": True}
     ],
     "5": [
-        {"username": "Kamren", "task": "Specific task for Kamren", "completed": False}
+        {"username": "Kamren", "task": "Expected task for Kamren", "completed": False}
     ],
     "6": [
-        {"username": "Leopoldo_Corkery", "task": "Specific task for Leopoldo_Corkery", "completed": False}
+        {"username": "Leopoldo_Corkery", "task": "Expected task for Leopoldo_Corkery", "completed": False}
     ],
     "7": [
-        {"username": "Elwyn.Skiles", "task": "Specific task for Elwyn.Skiles", "completed": True}
+        {"username": "Elwyn.Skiles", "task": "Expected task for Elwyn.Skiles", "completed": True}
     ],
     "8": [
-        {"username": "Maxime_Nienow", "task": "Specific task for Maxime_Nienow", "completed": False}
+        {"username": "Maxime_Nienow", "task": "Expected task for Maxime_Nienow", "completed": False}
     ],
     "9": [
-        {"username": "Delphine", "task": "Specific task for Delphine", "completed": True}
+        {"username": "Delphine", "task": "Expected task for Delphine", "completed": True}
     ],
     "10": [
-        {"username": "Moriah.Stanton", "task": "Specific task for Moriah.Stanton", "completed": False}
+        {"username": "Moriah.Stanton", "task": "Expected task for Moriah.Stanton", "completed": False}
     ]
 }
 
